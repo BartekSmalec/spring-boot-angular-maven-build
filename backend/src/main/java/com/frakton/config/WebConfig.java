2e7a68e1fe620dae5226f49ecc4fdb313e018bfc
Bartek <smalecbartlomiej@gmail.com>
2025-03-19 22:42:46 +0100

    Add some support for /mfa-ui/ path

--- backend/src/main/java/com/frakton/config/WebConfig.java
+++ backend/src/main/java/com/frakton/config/WebConfig.java
@@ -10,20 +10,32 @@ import java.io.IOException;
 
 public class WebConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
+        // Serve Angular app under /mfa-ui/
+        registry.addResourceHandler("/mfa-ui/**")
+                .addResourceLocations("classpath:/static/mfa-ui/")
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver() {
                     @Override
                     protected Resource getResource(String resourcePath, Resource location) throws IOException {
                         Resource requestedResource = location.createRelative(resourcePath);
+                        // If the requested resource exists, return it; otherwise, return the index.html
                         return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/static/index.html");
+                                : new ClassPathResource("/static/mfa-ui/index.html");
+                    }
+                });
+
+        // Fallback to serve index.html for any other paths (e.g., deep links in Angular routing)
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/mfa-ui/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        return new ClassPathResource("/static/mfa-ui/index.html");
                     }
                 });
     }
-
 }
